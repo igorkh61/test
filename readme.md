32776b55650e76c158d94cf4b42f737bf8779f16
Igor Khmelevtsov <igor@israelinfo.co.il>
2016-11-14 11:27:21 +0200

    start_branch commit 4

--- readme.md
+++ readme.md
@@ -1,3 +1,4 @@
 My first project for GIT
 This string from Windows
+start_branch commit 4
 
--- test1.php
+++ test1.php
@@ -2,4 +2,6 @@
 echo 'Hello from PHP!';
 echo '<br>It is my first project with GIT';
 echo '<br>This string from Windows';
+echo 'start_branch commit 4';
+
 ?>
